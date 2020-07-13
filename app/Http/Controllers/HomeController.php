7c6dd4dda29e8dee3257dc0ebf308194452d0cd3
Mina Nabil <mina9492@hotmail.com>
2020-07-14 00:45:28 +0200

    adding logout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
@@ -55,5 +56,12 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function logout()
+    {
+
+        Session::flush();
+        return redirect('login');
     }
 }
